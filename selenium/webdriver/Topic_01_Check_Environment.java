7d2948f34117073aae04956bd5c4fc6847c70026
grace3698 <anhln417@gmail.com>
2024-04-03 22:59:52 +0700

    Default checkbox

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -7,24 +7,35 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 public class Topic_01_Check_Environment {
-    WebDriver driver;
+    public static void main(String[] args) {
+        Random rand_number =new Random();
+        String email_address;
+        email_address = "automation" + rand_number.nextInt(99999)+"gmail.net";
 
-    @Test
-    public void TC_01_Run_On_Firefox() {
-        driver = new FirefoxDriver();
-        driver.get("https://sjc.com.vn/giavang/textContent.php");
 
-        String gia_ban = driver.findElement(By.xpath("//td[normalize-space()='Vàng nhẫn SJC 99,99 1 chỉ, 2 chỉ, 5 chỉ']/following-sibling::td[2]/span")).getText();
-        System.out.println(gia_ban);
-        driver.quit();
+
+        System.out.println("automation" + rand_number.nextInt(99999)+"gmail.net");
+        System.out.println("automation" + rand_number.nextInt(99999)+"gmail.net");
+        System.out.println("automation" + rand_number.nextInt(99999)+"gmail.net");
+        System.out.println("automation" + rand_number.nextInt(99999)+"gmail.net");
+
+
     }
+}
 
+
+//public class Topic_01_Check_Environment {
+//    WebDriver driver;
 //    @Test
-//    public void TC_02_Run_On_Chrome() {
-//        driver = new ChromeDriver();
+//    public void TC_01_Run_On_Firefox() {
+//        driver = new FirefoxDriver();
 //        driver.get("https://sjc.com.vn/giavang/textContent.php");
+//
+//        String gia_ban = driver.findElement(By.xpath("//td[normalize-space()='Vàng nhẫn SJC 99,99 1 chỉ, 2 chỉ, 5 chỉ']/following-sibling::td[2]/span")).getText();
+//        System.out.println(gia_ban);
 //        driver.quit();
 //    }
-
-}
+//}
